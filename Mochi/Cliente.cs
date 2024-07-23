1d80f30b3dc53cf6cdc7ed734f58b3ecf591f221
kimyenseo <131898516+kimyenseo@users.noreply.github.com>
2024-07-23 07:44:27 -0300

    Add files via upload

---
+++ Mochi/Cliente.cs
@@ -0,0 +1,17 @@
+namespace Mochi;
+public class cliente
+{
+string Nome;
+Int64 Senha;
+
+public void SetNome (string nome)
+{
+    this.Nome = Nome;
+}
+public String GetNome()
+{
+    return Nome;
+}
+
+
+}
