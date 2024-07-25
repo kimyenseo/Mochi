cc60227b66f033e19692935dd68d2cd7a6ae28fb
kimyenseo <131898516+kimyenseo@users.noreply.github.com>
2024-07-25 11:44:51 -0300

    Add files via upload

---
+++ Mochi-main/Mochi/ContaPagar.cs
@@ -0,0 +1,18 @@
+namespace Mochi;
+public class ContaPagar:Cliente
+{
+string validade;
+public void Setvalidade(String validade)
+{
+    this.validade = validade;
+}
+
+public string Getvalidade()
+
+{
+    return validade;
+}
+
+                                   
+                         
+}                                                               
