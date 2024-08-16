c6f9b8cdf53090f3650d293c127a8db50ae8a15d
Luana <luanamoraesgamaroni@gmail.com>
2024-08-16 08:59:17 -0300

    .

--- Mochi/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Mochi.AssemblyInfo.cs
+++ Mochi/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Mochi.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mochi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+57194c73f32d8e67c235581f947af46a766984f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d5a37a9bd25c78b1999389e5bf58604baef049e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mochi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mochi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
